dd28b88d3320d5cd281755ff6513c4ce54aeb7b1
Muahmamd <muhamamdibrohimov0306@gmail.com>
2026-05-12 11:23:33 +0500

    feat: integrate Firebase services, rename project, and update environment and ignore configurations

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_9jNSBy5Th0elj8HkjAFr9P8hqXdUELw",
-  authDomain: "test-bot-1f9e7.firebaseapp.com",
-  projectId: "test-bot-1f9e7",
-  storageBucket: "test-bot-1f9e7.firebasestorage.app",
-  messagingSenderId: "119758342157",
-  appId: "1:119758342157:web:106dac393ccf426f746d44",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
